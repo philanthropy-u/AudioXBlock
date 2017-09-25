f1bb36a298ce659a5d9b73021c970c99a228496a
Tariq Rasheed <tariq.rasheed@arbisoft.com>
2017-09-25 16:42:17 +0500

    Client side validation (#3)
    * client side validation completed

--- audio/audio.py
+++ audio/audio.py
@@ -6,6 +6,17 @@ from xblock.core import XBlock
 from xblock.fields import Scope, Integer, String
 from xblock.fragment import Fragment
 
+import re
+import requests
+
+regex = re.compile(
+        r'^(?:http|ftp)s?://' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|' #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
 
 class AudioXBlock(XBlock):
     """
@@ -16,24 +27,34 @@ class AudioXBlock(XBlock):
     # self.<fieldname>.
 
     # this variable holds the source of main media file
-    src = String(scope=Scope.settings, help="URL for .ogg file to play")
+    src = String(scope=Scope.settings, help="URL for .ogg file to play", default="")
     # reference for script file
     transcript_src = String(scope=Scope.settings, help="plain text", default="")
     # holds the downloadable link of media file
-    downloadable_src = String(scope=Scope.settings, help="URL for .mp3 file to download")
+    downloadable_src = String(scope=Scope.settings, help="URL for .mp3 file to download", default="")
 
     def resource_string(self, path):
         """Handy helper for getting resources from our kit."""
         data = pkg_resources.resource_string(__name__, path)
         return data.decode("utf8")
 
-    # TO-DO: change this view to display your data your own way.
     def student_view(self, context=None):
         """
         The primary view of the AudioXBlock, shown to students
         when viewing courses.
         """
         html = self.resource_string("static/html/audio.html")
+
+        # Validate transcript link.
+        if self.transcript_src:
+            if not regex.match(self.transcript_src):
+                self.transcript_src = 'error'
+            else:
+                r = requests.get(self.transcript_src)
+                content_type = r.headers['content-type']
+                if "text/plain" != content_type:
+                    self.transcript_src = 'error'
+
         frag = Fragment(html.format(src=self.src, transcript_src=self.transcript_src, downloadable_src=self.downloadable_src))
         frag.add_css(self.resource_string("static/css/audio.scss"))
         js = self.resource_string("static/js/src/audio.js")
@@ -47,7 +68,7 @@ class AudioXBlock(XBlock):
         """
         html = self.resource_string("static/html/audio_edit.html")
         frag = Fragment(html.format(src=self.src, transcript_src=self.transcript_src, downloadable_src=self.downloadable_src))
-
+        frag.add_css(self.resource_string("static/css/audio_edit.scss"))
         js = self.resource_string("static/js/src/audio_edit.js")
         frag.add_javascript(js)
         frag.initialize_js('AudioEditBlock')
@@ -65,7 +86,6 @@ class AudioXBlock(XBlock):
 
         return {'result': 'success'}
 
-    # TO-DO: change this to create the scenarios you'd like to see in the
     # workbench while developing your XBlock.
     @staticmethod
     def workbench_scenarios():
@@ -74,8 +94,11 @@ class AudioXBlock(XBlock):
             ("AudioXBlock",
              """<vertical_demo>
                     <audio src="https://upload.wikimedia.org/wikipedia/en/4/45/ACDC_-_Back_In_Black-sample.ogg" 
-                    transcript_src="http://norvig.com/big.txt"
-                    downloadable_src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"> </audio>
-                </vertical_demo>
+                    transcript_src="http://humanstxt.org/humans.txt"
+                    downloadable_src="sdfsdf.mp3"> </audio>
+                    <audio src="" 
+                    transcript_src=""
+                    downloadable_src=""> </audio>
+                 </vertical_demo>
              """),
         ]
--- audio/static/js/src/audio_edit.js
+++ audio/static/js/src/audio_edit.js
@@ -1,17 +1,19 @@
 function AudioEditBlock(runtime, element) {
-  $(element).find('.save-button').bind('click', function() {
-    var handlerUrl = runtime.handlerUrl(element, 'studio_submit');
-    var data = {
-      src: $(element).find('input[name=audio_src]').val(),
-      downloadable_src: $(element).find('input[name=audio_src_downloadable]').val(),
-      transcript_src: $(element).find('input[name=transcript_src]').val()
-    };
-    $.post(handlerUrl, JSON.stringify(data)).done(function(response) {
-      window.location.reload(false);
-    });
-  });
 
-  $(element).find('.cancel-button').bind('click', function() {
-    runtime.notify('cancel', {});
-  });
+    $(element).parents('.edit-xblock-modal').find('.action-save').unbind('click').bind('click', function(evt){
+        evt.preventDefault();
+        var handlerUrl = runtime.handlerUrl(element, 'studio_submit');
+        var data = {
+            src: $(element).find('input[name=audio_src]').val(),
+            downloadable_src: $(element).find('input[name=audio_src_downloadable]').val(),
+            transcript_src: $(element).find('input[name=transcript_src]').val()
+        };
+        $.post(handlerUrl, JSON.stringify(data)).done(function(response) {
+            runtime.notify('save', {state: 'end'});
+        });
+    });
+
+    $(element).find('.action-cancel').bind('click', function() {
+        runtime.notify('cancel', {});
+    });
 }
