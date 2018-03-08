18a751deb90f390ad474d9559cb7f488066acee0
Mohammad Nadeem <muhammad.nadeem@arbisoft.com>
2018-03-08 15:59:00 +0500

    Mnadeem/safari iphone playbackfix (#18)

--- audio/static/js/src/audio.js
+++ audio/static/js/src/audio.js
@@ -154,6 +154,12 @@ function AudioXBlock(runtime, element) {
         seekbar[0].min = audio[0].startTime;
         seekbar[0].max = audio[0].duration;
         seekbar[0].value = audio[0].currentTime;
+
+        loaderIcon.removeClass('has-loader')
+    });
+
+    audio.bind("playing", function () {
+      loaderIcon.addClass('has-loader')
     });
 
     audio.bind("playing", function () {
